6b6e1cc16d27b84154c6f95b74b6e42c3fb4475f
Kenny Nguyen <baterra602@gmail.com>
2019-10-01 18:43:54 -0700

    Added a test enemy that shoots a test bullet every 4 seconds. No enemy movement yet.

---
+++ Subcircle game fall 2019/Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public float health;
+    public float speed;
+    public float bulletSpeed;
+
+    public abstract void shoot();
+    public abstract void move();
+}
---
+++ Subcircle game fall 2019/Assets/Scripts/SidewaysEnemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SidewaysEnemy : Enemy
+{
+    public GameObject bulletPrefab;
+
+    public Transform shootPoint; // the position where the bullet will spawn
+
+    private bool isCooldown = false;// whether or not the shooting ability is on cooldown
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shoot(); 
+    }
+
+
+
+    public void ResetShootCooldown()
+    {
+        isCooldown = false;
+    }
+
+    override
+    public void shoot() // shoots a bullet with some force
+    {
+       // Debug.Log(!isCooldown);
+        if (!isCooldown) // only shoot when there is no cooldown
+        {
+            Debug.Log("Shoot2");
+            isCooldown = true;
+            GameObject projectile = GameObject.Instantiate(bulletPrefab); // spawn the bullet
+            
+
+            projectile.transform.position = shootPoint.position; // set location of made bullet 
+                                                                 //to shootpoint location
+            Debug.Log(shootPoint.position);
+            projectile.GetComponent<Rigidbody2D>().velocity = new Vector3(0, -bulletSpeed); // give the bullet speed
+
+            Invoke("ResetShootCooldown", 2f); // reset the shoot cooldown after some time
+        }
+    }
+
+
+
+    override
+    public  void move()
+    {
+
+    }
+
+}
