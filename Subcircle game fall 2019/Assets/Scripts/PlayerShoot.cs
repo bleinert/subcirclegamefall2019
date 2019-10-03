a255d753e3d264439500a1ac8f1e978ea87f3088
bleinert <bleinert@gmail.com>
2019-10-02 23:39:06 -0700

    added scene for testing bullets, and relataed test player and bullet prefabs.

---
+++ Subcircle game fall 2019/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public Vector3 launchPoint;
+    public Transform trans;
+   
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+ 
+            Instantiate(bullet,(trans.position+launchPoint),transform.rotation);
+
+
+
+        }
+    }
+    
+}
