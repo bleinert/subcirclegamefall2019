58aba767b21cb3815ee292c32b3bc7fcb9de9c8c
KingdomCross <alexchheng1998@yahoo.com>
2019-10-17 15:29:40 -0700

    BG Scroller complete

---
+++ Subcircle game fall 2019/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://www.youtube.com/watch?v=32EIYs6Z18Q 
+public class BGScroller : MonoBehaviour
+{
+    public float bgSpeed;
+    public Renderer bgRender;
+
+    // Update is called once per frame
+    void Update()
+    {
+        VerticalScroll();
+    }
+
+    void VerticalScroll()
+    {
+        bgRender.material.mainTextureOffset += new Vector2(0f, bgSpeed * Time.deltaTime);
+    }
+}
