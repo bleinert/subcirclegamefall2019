0594e9957795b7adf090b023aef05edf52f15f87
Kenny Nguyen <baterra602@gmail.com>
2019-10-24 14:43:32 -0700

    1st attempt at pathfinding. Ai currently can follow the path of nodes and if it reaches the end of the path, it will back the other way using the same path. Movement is janky and the script for it needs to be refined.

--- Subcircle game fall 2019/Assets/Scripts/SidewaysEnemy.cs
+++ Subcircle game fall 2019/Assets/Scripts/SidewaysEnemy.cs
@@ -13,6 +13,10 @@ public class SidewaysEnemy : Enemy
 
     private bool isCooldown = false;// whether or not the shooting ability is on cooldown
 
+    public List<Transform> pathNodes = new List<Transform>();
+    private int nodeIndex = 0;
+    private bool isReversePath = false; // whether the ai should move in the opposite direction of the pathway
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +26,7 @@ public class SidewaysEnemy : Enemy
     // Update is called once per frame
     void Update()
     {
-        shoot();
+        //shoot();
         move();
       
     }
@@ -40,14 +44,14 @@ public class SidewaysEnemy : Enemy
        // Debug.Log(!isCooldown);
         if (!isCooldown) // only shoot when there is no cooldown
         {
-            Debug.Log("Shoot2");
+            //Debug.Log("Shoot2");
             isCooldown = true;
             GameObject projectile = GameObject.Instantiate(bulletPrefab); // spawn the bullet
             
 
             projectile.transform.position = shootPoint.position; // set location of made bullet 
                                                                  //to shootpoint location
-            Debug.Log(shootPoint.position);
+            //Debug.Log(shootPoint.position);
             projectile.GetComponent<Rigidbody2D>().velocity = new Vector3(0, -bulletSpeed); // give the bullet speed
 
             Invoke("ResetShootCooldown", 2f); // reset the shoot cooldown after some time
@@ -59,24 +63,63 @@ public class SidewaysEnemy : Enemy
     override
     public  void move()
     {
-     
+
         // this.transform.position = new Vector3(Mathf.PingPong
         //  (Time.time * 2, xRightBoundry - xLeftBoundry) + xLeftBoundry,
         //  this.transform.position.y, this. transform.position.z);
 
-        if (this.transform.position.x > xRightBoundry && speed > 0) //if enemy is moving right and past the 
-                                                                    // right boundry
+        if (pathNodes != null) // if there is a path to follow, do the following: 
         {
-            speed *= -1;// change its moving direction to left
+            
+            if (Vector2.Distance(this.transform.position, pathNodes[nodeIndex].position) < 1f) // if the enemy's position reaches the first node
+                                                                             // in the pathway, start following the next node
+                                                                             //Only compare x and y coords and not z coords
+            {
+                
+                if (isReversePath)  // this block segment gets the AI's next path to follow in reverse order
+                {
+                    if (nodeIndex > 0)
+                    {
+                        nodeIndex--; // get next node if enemy has not reach end of its path
+                    }
+                    else //  if it reaches the end of its path, make the enemy start moving the other way
+                    {
+                        isReversePath = false;
+                        nodeIndex++;
+                    }
+                }
+
+
+                else // this block segment gets the AI's next path to follow in non reverse order
+                {
+                    if (nodeIndex < pathNodes.Count-1)
+                    {
+                        nodeIndex++; // get next node if enemy has not reach end of its path
+                    }
+
+                    else //  if it reaches the end of its path, make the enemy start moving the other way
+                    {
+                        isReversePath = true;
+                        nodeIndex--;
+                    }
+                }
+
+                Debug.Log(nodeIndex);
+
+            }
+            else // if enemy has not reach its next node in the path, make it move a step closer to that node
+            {
+               
+               
+                this.transform.position += new Vector3 ((pathNodes[nodeIndex].position.x - this.transform.position.x > 0) ?
+                    Time.deltaTime : (Time.deltaTime * -1),0,0 );
+
+                this.transform.position += new Vector3(0,(pathNodes[nodeIndex].position.y - this.transform.position.y > 0) ?
+                    Time.deltaTime : (Time.deltaTime * -1), 0);
+            }
         }
 
-        if (this.transform.position.x < xLeftBoundry && speed < 0)//if enemy is moving left and past the 
-                                                                  // left boundry
-        {
-            speed *= -1; // change its moving direction to right
-        }
 
-        this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0); // move enemy based on speed
 
     }
     private void OnTriggerStay2D(Collider2D collision)
