626d19e1d96b106eed3e28b9f60954f68275fbe6
JTanXsol <tjohnnaidi@gmail.com>
2019-11-20 21:58:34 -0800

    Got the Game Over Menu to Function

---
+++ Subcircle game fall 2019/Assets/GameOverUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverUI : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("Application Quit");
+        Application.Quit(); //Quits Game
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); // Should Start the game again
+    }
+}
