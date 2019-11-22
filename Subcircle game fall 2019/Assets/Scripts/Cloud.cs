6377eea8939882d3b52e06cbc4936cdbfc28c30e
Kenny Nguyen <baterra602@gmail.com>
2019-11-22 00:14:52 -0800

    no message

---
+++ Subcircle game fall 2019/Assets/Scripts/Cloud.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : Enemy
+{
+    public AudioClip EnemySound;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //shoot();
+        move();
+    }
+
+    override
+    public void move()
+    {
+        this.GetComponentInParent<Transform>().position += new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+
+    override
+    public void shoot()
+    {
+
+    }
+
+
+   
+    override
+    public void loseHealth(float value)
+    {
+       
+    }
+
+
+
+}
---
+++ Subcircle game fall 2019/Assets/Scripts/WinConditon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinConditon : MonoBehaviour
+{
+    public List<Transform> spawners = new List<Transform>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
